2919046fbb9b3ee3d68a12f03ba150619520ae79
Rolando Garcia <rogarcia@berkeley.edu>
2023-12-13 15:41:08 -0600

    Add flex layout and containers for PDF and metadata display

---
+++ featurize.py
@@ -0,0 +1,19 @@
+from app import PDF_DIR, TXT_DIR
+
+import re
+
+
+def analyze_text(text_file):
+    with open(text_file, 'r') as file:
+        text = file.read()
+
+    # Example pattern for detecting headings (e.g., all caps)
+    headings = re.findall(r'^[A-Z\s]+$', text, re.MULTILINE)
+
+    # Example pattern for detecting page numbers
+    page_numbers = re.findall(r'\b\d+\b', text)  # Simplistic; needs refinement
+
+    # Other analysis can be added here
+
+    return headings, page_numbers 
+
