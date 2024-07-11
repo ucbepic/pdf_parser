548a6b4d8a0cd1848384791e0c3617634adefa02
Rolando Garcia <rogarcia@berkeley.edu>
2024-07-11 06:32:08 -0500

    label_by_hand top layer: v1, v2, appendix

--- label_by_hand.py
+++ label_by_hand.py
@@ -11,14 +11,7 @@ documents = sorted(
 )
 
 # 0-indexed first pages of each document
-first_page = {
-    "6-Complaint__Judgment": [0, 13, 14],
-    "AmericanHistoriansAmicus": [0],
-    "AppealsPerCuriam_NoAbsoluteImmunity": [0],
-    "Menlo_Ave_9Jul20": [0, 9, 10, 16],
-    "NY_Indictment_StormyD": [0, 15, 16],
-    "george_santos_report": [0],
-}
+first_page = {"mueller_report": [0, 207, 394]}
 
 
 if __name__ == "__main__":
